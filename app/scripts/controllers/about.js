fc13ba14479da8e9ea25eef015107618884fb378
Poonam Baveja <poonambaveja@gmail.com>
2016-03-08 14:54:26 +0530

    added files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newFolderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newFolderApp
+ */
+angular.module('newFolderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newFolderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newFolderApp
+ */
+angular.module('newFolderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
